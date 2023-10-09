d45ad9c99c9416fdf7846dae4a6559438fbe0193
Pedro Soares Dias <pedroferraridias@gmail.com>
2023-10-09 14:59:48 -0300

    Criando o Componente Inicio

---
+++ src/Components/Inicio.jsx
@@ -0,0 +1,10 @@
+export default function Inicio() {
+    return (
+        <section className="flex  justify-center items-center min-h-screen bg-gradient-to-tr from-gray-900 to-violet-800">
+            <div className="text-white text-center">
+                <h1 className="container sm:text-xl text-base font-semibold p-2">Olá, eu sou Pedro Dias</h1>
+                <p className="container sm:text-lg text-sm font-semibold p-2">Trabalho com criação sites com o maior desempenho e com a boa acessibilidade</p>
+            </div>
+        </section>
+    )
+}
