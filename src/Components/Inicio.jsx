4e62f30a8138a57bdfd46b07c17dd1b9f82dc26a
Pedro Soares Dias <pedroferraridias@gmail.com>
2023-11-10 07:28:56 -0300

    Pequena modificação no componente Inicio, e atualizando o Next.JS na versão 14

--- src/Components/Inicio.jsx
+++ src/Components/Inicio.jsx
@@ -1,9 +1,9 @@
 export default function Inicio() {
     return (
-        <section className="flex  justify-center items-center min-h-screen bg-gradient-to-tr from-gray-900 to-violet-800">
-            <div className="text-white text-center">
+        <section className="flex justify-center items-center min-h-screen bg-gradient-to-tr from-gray-900 to-purple-800">
+            <div className="container text-white text-center">
                 <h1 className="container sm:text-xl text-base font-semibold p-2">Olá, eu sou Pedro Dias</h1>
-                <p className="container sm:text-lg text-sm font-semibold p-2">Trabalho com criação sites com o maior desempenho e com a boa acessibilidade</p>
+                <p className="container sm:text-lg text-sm font-semibold px-9">Como um desenvolvedor web, meu objetivo é criar sites de excelente qualidade, com ótimo desempenho, responsividade, facilidade de uso e acessibilidade.</p>
             </div>
         </section>
     )
